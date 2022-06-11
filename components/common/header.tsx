f4f460cfc39610bfef442b5377bbb364fdda1949
qzart45hi <qwert45hi@yandex.ru>
2022-06-11 21:13:19 +0300

    feat: upgrade header for future settings drawer

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,12 +1,14 @@
-import { AppBar, Button, Stack, Toolbar } from "@mui/material"
-import { useRouter } from "next/router"
+import { useState } from "react"
+import { AppBar, Button, Grid, Stack, SwipeableDrawer, Toolbar } from "@mui/material"
 import { useTranslation } from "next-i18next"
+import { Settings as SettingsIcon, ContactSupport as SupportIcon, Logout as LogoutIcon } from "@mui/icons-material"
 
 import { useAppSelector } from "../../data/hooks"
 import { signOut } from "../../data/slices/moderator"
 import { authorizedFetch } from "../../utils/fetcher"
 import { useRequestorBase } from "../../utils/requestor"
 import { Link } from "./navigation"
+import { TooltipIconButton } from "./library"
 
 interface HeaderButtonProps {
   text: string
@@ -35,55 +37,86 @@ function HeaderItem({ href, text, path, selected }: HeaderItemProps) {
   </Link>
 }
 
-function AuthorizedHeader() {
+export default function Header() {
+  const { t } = useTranslation("common")
+
+  const authorized = useAppSelector(state => state.moderator.authorized)
+  const permissions = useAppSelector(state => state.moderator.permissions)
+
   const { router, dispatch } = useRequestorBase()
   const path: string = router.asPath
 
-  const permissions = useAppSelector(state => state.moderator.permissions)
+  function doSignOut() {
+    authorizedFetch("/sign-out/", { method: "post" })
+    dispatch(signOut())
+    router.push("/signin")
+  }
+
+  const [open, setOpen] = useState<boolean>(true)
 
   return <AppBar position="fixed" enableColorOnDark>
+    <SwipeableDrawer
+      anchor="right"
+      open={open}
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+    >
+      hi
+    </SwipeableDrawer>
     <Toolbar variant="dense">
-      <Stack direction="row" sx={{ width: "25%" }}>
-        <HeaderItem href="/" path={path} text="header-home" />
-      </Stack>
-      <Stack direction="row" sx={{ width: "50%", justifyContent: "center" }}>
-        {router.asPath !== "/" && permissions?.map((item, key) =>
-          <HeaderItem
-            text={item.name}
-            key={key}
-            path={path}
-            href={`/categories/${item.name.replace(" ", "-")}`}
-            selected={path.startsWith(`/categories/${item.name.replace(" ", "-")}`)}
-          />
-        )}
-      </Stack>
-      <Stack direction="row-reverse" sx={{ width: "25%" }}>
-        <HeaderItem text="support" href="/support" path={path} />
-        <HeaderButton text="log out" onClick={() => {
-          authorizedFetch("/sign-out/", { method: "post" })
-          dispatch(signOut())
-          router.push("/signin/")
-        }} />
-      </Stack>
+      <Grid
+        container
+        direction="row"
+        width="100%"
+      >
+        {authorized
+          ? <>
+            <Grid item xs={2}>
+              <HeaderItem href="/" path={path} text="header-home" />
+            </Grid>
+            <Grid item xs>
+              <Stack direction="row" sx={{ width: "100%", justifyContent: "center" }}>
+                {router.asPath !== "/" && permissions?.map((item, key) =>
+                  <HeaderItem
+                    text={item.name}
+                    key={key}
+                    path={path}
+                    href={`/categories/${item.name.replace(" ", "-")}`}
+                    selected={path.startsWith(`/categories/${item.name.replace(" ", "-")}`)}
+                  />
+                )}
+              </Stack>
+            </Grid>
+          </>
+          : <Grid item xs>
+            <Stack direction="row" sx={{ width: "100%" }}>
+              <HeaderItem text="sign in" href="/signin" path={path} />
+            </Stack>
+          </Grid>
+        }
+        <Grid item xs={2}>
+          <Stack direction="row-reverse">
+            <TooltipIconButton
+              sx={{ ml: 0.5 }}
+              Icon={SettingsIcon}
+              title={t("header-tooltip-settings")}
+              onClick={() => setOpen(!open)}
+            />
+            <Link href="/support">
+              <TooltipIconButton
+                sx={{ ml: 0.5, color: path === "/support" ? "secondary.main" : "common.white" }}
+                Icon={SupportIcon}
+                title={t("header-tooltip-support")}
+              />
+            </Link>
+            {authorized && <TooltipIconButton
+              Icon={LogoutIcon}
+              title={t("header-tooltip-signout")}
+              onClick={doSignOut}
+            />}
+          </Stack>
+        </Grid>
+      </Grid>
     </Toolbar>
   </AppBar>
-}
-
-function OutsiderHeader() {
-  const router = useRouter()
-  const path: string = router.asPath
-
-  return <AppBar position="fixed" enableColorOnDark>
-    <Toolbar variant="dense">
-      <Stack direction="row-reverse" sx={{ width: "100%" }}>
-        <HeaderItem text="support" href="/support" path={path} />
-        <HeaderItem text="sign in" href="/signin" path={path} />
-      </Stack>
-    </Toolbar>
-  </AppBar>
-}
-
-export default function Header() {
-  const authorized = useAppSelector(state => state.moderator.authorized)
-  return authorized ? <AuthorizedHeader /> : <OutsiderHeader />
 }
